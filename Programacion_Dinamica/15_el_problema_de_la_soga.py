5db58cc5ec0ff43a77141b449fad1c8511742b3d
Isaias <isaiascabrera20@gmail.com>
2024-10-19 02:11:28 -0300

    Se agrega el ejercicio 15

--- Programacion_Dinamica/15_el_problema_de_la_soga.py
+++ Programacion_Dinamica/15_el_problema_de_la_soga.py
@@ -17,4 +17,35 @@ n = 10 --> Debe devolver 36 (producto máximo es 3 * 3 * 4)
 """
 
 def problema_soga(n):
-    return 0
+    
+    longitud_soga = n
+
+    if longitud_soga == 2:
+        return 1
+    if longitud_soga == 3:
+        return 2
+    maximo_producto = [0] * (longitud_soga + 1)
+    maximo_producto[1], maximo_producto[2], maximo_producto[3] = 1, 2, 3
+
+    for i in range(4, longitud_soga + 1):
+        for j in range(1, i // 2 + 1):
+            maximo_producto[i] = max(maximo_producto[i], maximo_producto[j] * maximo_producto[i - j])
+    return maximo_producto[longitud_soga]
+
+def main():
+    print("Probando la función problema_soga:")
+    print("n = 2:", problema_soga(2))
+    print("n = 3:", problema_soga(3))
+    print("n = 4:", problema_soga(4))
+    print("n = 5:", problema_soga(5))
+    print("n = 6:", problema_soga(6))
+    print("n = 7:", problema_soga(7))
+    print("n = 8:", problema_soga(8))
+    print("n = 9:", problema_soga(9))
+    print("n = 10:", problema_soga(10))
+    print("n = 20:", problema_soga(20))
+    print("n = 50:", problema_soga(50))
+
+if __name__ == "__main__":
+    main()
+
