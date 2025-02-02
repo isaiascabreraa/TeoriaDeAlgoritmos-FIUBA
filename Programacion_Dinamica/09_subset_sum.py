7245e21ea064ebaf77015eb0a799a023889f12e5
Isaias <isaiascabrera20@gmail.com>
2025-02-02 19:00:41 -0300

    Se agregan problemas de PD (9 y 10)

--- Programacion_Dinamica/09_subset_sum.py
+++ Programacion_Dinamica/09_subset_sum.py
@@ -5,19 +5,62 @@ Problema 09: Tenemos un conjunto de números v_1, v_2, ... , v_n, y queremos obt
 tratando de aproximarse lo más posible a V. Implementar un algoritmo que reciba un arreglo de valores, y la suma objetivo V, y devuelva qué elementos deben ser utilizados 
 para aproximar la suma lo más posible a V, sin pasarse. Indicar y justificar la complejidad del algoritmo implementado.
 
-Resolucion:
+Resolucion: Lo que hago inicialmente es generar una matriz de N * V en la cual voy probando caso a caso si agrego o no el elemento para diferentes valores menores o iguales 
+a V. Por ejemplo, inicio considerando si debo de agregar el primer elemento cuando V = 0, luego se lo agrego cuando V = 1 y asi hasta que V = V. Luego continuo con el
+elemento siguiente y asi sucesivamente. En caso de no poder agregar el elemento me quedo con el optimo cuando habia un elemento menos (Optimo[i - 1][j]) y si puedo agregarlo
+entonces uso el valor mas grande entre el optimo anterior (Optimo[i - 1][j]) y el valor obtenido de el elemento actual + el optimo que habia en la posicion que resulta de
+restar a la columna actual el valor del elemento actual (Optimo[i - 1][j - elemento_actual]).
 
-La complejidad algoritmica es del orden de:
+Mi ecuacion de recurrencia resulta de la forma: SUBSET_OPTIMOS[i][j] = max(SUBSET_OPTIMOS[i - 1][j], SUBSET_OPTIMOS[i - 1][j - elementos[i - 1]] + elementos[i - 1]), en donde
+para recontruir la solucion recorro la matriz obtenida (empezando por [N][V]) y verifico si la posicion actual i vale lo mismo que el optimo con un elemento menos (i - 1) y si 
+es asi entonces retrocedo una fila (i -= 1) y sigo probando hasta encontrar encontrar el elemento que difiera del optimo anterior (Optimo[i][j] != Optimo[i - 1][j])) ya que 
+ese será el que debemos utilizar, y una vez encontrado agregamos el valor de elemento[i - 1] a nuestra solucion y seguiremos con la fila anterior y la columna determinada
+por Optimos[i - 1][j - elementos[i - 1]].
+
+La complejidad algoritmica es del orden de: O(n * V)
 """
 
 
+def reconstruir_subconjunto(elementos, dp, V):
+
+    resultado = []
+    i, j = len(elementos), V
+
+    while i > 0 and j > 0:
+        if dp[i][j] != dp[i - 1][j]:
+            resultado.append(elementos[i - 1])
+            j -= elementos[i - 1]
+        i -= 1
+
+    return resultado[::-1]
+
+
 def subset_sum(elementos, v):
-    return []
+
+    subset_obtenido = subset_sum_dinamico(elementos, v)
+    return reconstruir_subconjunto(elementos, subset_obtenido, v)
+
+
+def subset_sum_dinamico(elementos, v):
+
+    n = len(elementos)
+    SUBSET_OPTIMOS = [[0] * (v + 1) for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for j in range(v + 1):
+
+            if elementos[i - 1] > j:
+                SUBSET_OPTIMOS[i][j] = SUBSET_OPTIMOS[i - 1][j]  # No podemos incluir este elemento
+            else:
+                SUBSET_OPTIMOS[i][j] = max(SUBSET_OPTIMOS[i - 1][j], SUBSET_OPTIMOS[i - 1][j - elementos[i - 1]] + elementos[i - 1])
+
+    return SUBSET_OPTIMOS
+
 
 def main():
 
-    elementos = [3, 34, 4, 12, 5, 2]
-    V = 10
+    elementos = [3, 4, 12, 5, 2, 10]
+    V = 19
 
     resultado = subset_sum(elementos, V)
     print(f"{resultado}")
---
+++ Programacion_Dinamica/10_tu_a_londres_y_yo_a_california.py
@@ -0,0 +1,73 @@
+
+"""
+Problema 10: Manejamos un negocio que atiende clientes en Londres y en California. Nos interesa cada mes decidir si operar en una u otra ciudad. Los costos de operación para 
+cada mes pueden variar y son dados por 2 arreglos: L y C, con valores para todos los meses hasta n. Naturalmente, si en un mes operamos en una ciudad, y al siguiente en una 
+distinta, habrá un costo fijo M por la mudanza. Dados los arreglos de costos de operación en Londres (L) y California (C), indicar la secuencia de las n localizaciones en las 
+que operar durante los n meses, sabiendo que queremos minimizar el total de los costos de operación. Se puede empezar en cualquier ciudad. Indicar y justificar la complejidad 
+del algoritmo implementado.
+
+Resolucion:
+
+La complejidad algoritmica es del orden de:
+"""
+
+
+def reconstruir_solucion(L, C, M, costos_obtenidos):
+
+    n = len(L)
+    ciudades_seleccionadas = []
+    
+    # Determino la última ciudad con menor costo
+    if costos_obtenidos[n-1][0] <= costos_obtenidos[n-1][1]:
+        ciudad_actual = 0  # Londres
+    else:
+        ciudad_actual = 1  # California
+
+    for i in range(n-1, -1, -1):
+        ciudades_seleccionadas.append("L" if ciudad_actual == 0 else "C")
+        if i > 0:
+
+            if ciudad_actual == 0:  # Si estamos en Londres
+                if costos_obtenidos[i][0] == costos_obtenidos[i-1][1] + M + L[i]:  
+                    ciudad_actual = 1  # Cambiamos a California
+
+            else:  # Si estamos en California
+                if costos_obtenidos[i][1] == costos_obtenidos[i-1][0] + M + C[i]:  
+                    ciudad_actual = 0  # Cambiamos a Londres
+
+    return ciudades_seleccionadas[::-1]
+
+
+def funcion(L, C, M):
+
+    costos_obtenidos = funcion_dinamica(L, C, M)
+    return reconstruir_solucion(L, C, M, costos_obtenidos)
+
+
+def funcion_dinamica(L, C, M):
+
+    n = len(L)
+    
+    COSTOS_OPTIMOS = [[0] * 2 for _ in range(n)]
+    
+    COSTOS_OPTIMOS[0][0] = L[0]
+    COSTOS_OPTIMOS[0][1] = C[0]
+
+    for i in range(1, n):
+        COSTOS_OPTIMOS[i][0] = min(COSTOS_OPTIMOS[i-1][0] + L[i], COSTOS_OPTIMOS[i-1][1] + M + L[i])
+        COSTOS_OPTIMOS[i][1] = min(COSTOS_OPTIMOS[i-1][1] + C[i], COSTOS_OPTIMOS[i-1][0] + M + C[i])
+
+    return COSTOS_OPTIMOS
+
+
+def main():
+    
+    L = [3, 2, 7, 4, 6, 8, 9, 0, 2, 4, 6, 10]  # Costos en Londres
+    C = [5, 1, 3, 9, 2, 3, 7, 4, 9, 6, 1, 10]  # Costos en California
+    M = 4  # Costo de mudanza entre ciudades
+
+    resultado = funcion(L, C, M)
+    print(f"{resultado}")
+
+if __name__ == '__main__':
+    main()
