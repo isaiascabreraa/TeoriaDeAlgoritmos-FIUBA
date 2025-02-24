9dd580e447a143eb10fd994df8402269349d3509
Isaias <isaiascabrera20@gmail.com>
2025-02-24 13:08:27 -0300

    Correccion orden topologico

--- 03_Back_Tracking/18_ordenamientos_topologicos.py
+++ 03_Back_Tracking/18_ordenamientos_topologicos.py
@@ -16,25 +16,29 @@ def obtener_predecesores(grafo, v):
     #Retorna los predecesores de un vértice en un grafo dirigido.
     return [w for w in grafo.obtener_vertices() if grafo.estan_unidos(w, v)]
 
-def contar_ordenamientos_bt(grafo, orden_actual, visitados, orden_total, vertices):
+def contar_ordenamientos_bt(grafo, orden_actual, visitados, orden_total, vertices, vertices_actualizados):
     #Genera todos los ordenamientos topológicos posibles por backtracking.
     
     if len(orden_actual) == len(vertices):
         orden_total.append(orden_actual[:])
         return
+    
+    vertices_actualizados = vertices[:]
 
-    for v in vertices:
+    for v in vertices[:]:
     
         if v not in visitados and all(predecesor in visitados for predecesor in obtener_predecesores(grafo, v)):
         #El vertice V solo se agrega si aun no fue visitado y si todos sus predecesores ya fueron agregados al orden.
-        
+
             visitados.add(v)
             orden_actual.append(v)
+            vertices_actualizados.remove(v)
 
-            contar_ordenamientos_bt(grafo, orden_actual, visitados, orden_total, vertices)
+            contar_ordenamientos_bt(grafo, orden_actual, visitados, orden_total, vertices, vertices_actualizados)
 
             visitados.remove(v)
             orden_actual.pop()
+            vertices_actualizados.append(v)
 
 def contar_ordenamientos(grafo):
     if not grafo:
@@ -43,12 +47,7 @@ def contar_ordenamientos(grafo):
     orden_total = []
     visitados = set()
     vertices = grafo.obtener_vertices()
-
-    for vertice in vertices:
-        if all(predecesor in visitados for predecesor in obtener_predecesores(grafo, vertice)): #Si todos los predecesores fueron visitados...
-            visitados.add(vertice)
-            contar_ordenamientos_bt(grafo, [vertice], visitados, orden_total, vertices)
-            visitados.remove(vertice)
+    contar_ordenamientos_bt(grafo, [], visitados, orden_total, vertices, vertices)
 
     return len(orden_total)
 
@@ -64,9 +63,11 @@ def generar_grafo(vertices):
     grafo = Grafo(True, vertices)  # Grafo dirigido
     grafo.agregar_arista('A', 'B')
     grafo.agregar_arista('A', 'C')
+    grafo.agregar_arista('A', 'F')
     grafo.agregar_arista('B', 'D')
     grafo.agregar_arista('C', 'D')
     grafo.agregar_arista('D', 'E')
+    grafo.agregar_arista('F', 'E')
     return grafo
 
 
